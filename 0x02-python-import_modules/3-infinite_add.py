50176f198db55dea8a754f9df8071dd4a766bf76
Samuelsimwata <simwatasamuel@gmail.com>
2022-12-08 09:06:00 -0800

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the additon of all arguments."""
+    """Print the addition of all arguments."""
     import sys
 
     total = 0
